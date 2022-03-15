d190a62a00f22023bb381387d11f9247ccf88b94
Paul Kern <pkern@laas.fr>
2022-03-15 15:49:48 +0100

    Added more parameters check for spells

--- spell.py
+++ spell.py
@@ -47,12 +47,32 @@ class Spell():
             if not key in json_data:
                 raise KeyError(f"JSON string does not contain a '{key}' key.")
         
+        if not (isinstance(json_data['crit_chance'], float) or isinstance(json_data['crit_chance'], int)):
+            raise TypeError(f"json_data['crit_chance'] is not a float ('{json_data['crit_chance']}' of type '{type(json_data['crit_chance'])}' given instead).")
+        if not (0.0 <= json_data['crit_chance'] <= 1.0):
+            raise ValueError(f"json_data['crit_chance'] should be between 0 and 1 inclusive ('{json_data['crit_chance']}' given instead).")
+        
+        if not isinstance(json_data['uses_per_target'], int):
+            raise TypeError(f"json_data['uses_per_target'] is not a int ('{json_data['uses_per_target']}' of type '{type(json_data['uses_per_target'])}' given instead).")
+        if json_data['uses_per_target'] == 0 or json_data['uses_per_target'] < -1:
+            raise ValueError(f"json_data['uses_per_target'] should be -1 or a positive int ('{json_data['uses_per_target']}' given instead).")
+
+        if not isinstance(json_data['uses_per_turn'], int):
+            raise TypeError(f"json_data['uses_per_turn'] is not a int ('{json_data['uses_per_turn']}' of type '{type(json_data['uses_per_turn'])}' given instead).")
+        if json_data['uses_per_turn'] == 0 or json_data['uses_per_turn'] < -1:
+            raise ValueError(f"json_data['uses_per_turn'] should be -1 or a positive int ('{json_data['uses_per_turn']}' given instead).")
+        
         for characteristic in Characteristics:
             if not characteristic in json_data['damages']:
                 raise KeyError(f"JSON string 'damages' array does not contains '{characteristic}'.")
             for field in ('min', 'max', 'crit_min', 'crit_max'):
                 if not field in json_data['damages'][characteristic]:
                     raise KeyError(f"Field '{field}' missing in json_data['damages'][{characteristic}].")
+                field_value = json_data['damages'][characteristic][field]
+                if not isinstance(field_value, int):
+                    raise TypeError(f"json_data['damages'][{characteristic}][{field}] is not an int ('{field_value}' of type '{type(field_value)}' given instead).")
+                if field_value < 0:
+                    raise ValueError(f"json_data['damages'][{characteristic}][{field}] should be non negative ('{field_value}' given instead).")
         
 
     @classmethod
@@ -62,5 +82,8 @@ class Spell():
 
         stats = Spell(from_scratch=False)
         stats.damages = json_data['damages']
+        stats.crit_chance = float(json_data['crit_chance'])
+        stats.uses_per_target = json_data['uses_per_target']
+        stats.uses_per_turn = json_data['uses_per_turn']
 
         return stats
