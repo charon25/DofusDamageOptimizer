d74a3db3964ededd068c3cf594b62f02f2f3d2a2
Paul Kern <pkern@laas.fr>
2022-03-15 16:01:04 +0100

    Replaced 'damages' field with 'base_damages'

--- spell.py
+++ spell.py
@@ -5,7 +5,7 @@ from stats import Characteristics
 
 class Spell():
     def __init__(self, from_scratch=True) -> None:
-        self.damages = {}
+        self.base_damages = {}
         self.crit_chance = 0.0
         self.uses_per_target = -1
         self.uses_per_turn = -1
@@ -15,30 +15,32 @@ class Spell():
     
     def _fill_empty_dict(self):
         for characteristic in Characteristics:
-            self.damages[characteristic] = {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0}
+            self.base_damages[characteristic] = {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0}
 
-    def get_damages(self, characteristic):
+
+    def get_base_damages(self, characteristic):
         if not isinstance(characteristic, Characteristics):
             raise TypeError(f"'{characteristic} is not a valid characteristic.")
         
-        return self.damages[characteristic]
+        return self.base_damages[characteristic]
     
-    def set_damages(self, characteristic, damages):
+    def set_base_damages(self, characteristic, base_damages):
         if not isinstance(characteristic, Characteristics):
             raise TypeError(f"'{characteristic} is not a valid characteristic.")
         
-        if not isinstance(damages, dict):
-            raise TypeError(f"Damages is not a dict ('{damages}' of type '{type(damages)}' given instead).")
+        if not isinstance(base_damages, dict):
+            raise TypeError(f"base_damages is not a dict ('{base_damages}' of type '{type(base_damages)}' given instead).")
         
         for field in ('min', 'max', 'crit_min', 'crit_max'):
-            if not field in damages:
-                raise KeyError(f"Field '{field}' missing in damages.")
-            if not isinstance(damages[field], int):
-                raise TypeError(f"Field '{field}' is not an int ('{damages[field]}' of type '{type(damages[field])}' given instead).")
-            if damages[field] < 0:
-                raise ValueError(f"Field '{field}' should be non negative ('{damages[field]}' given instead).")
+            if not field in base_damages:
+                raise KeyError(f"Field '{field}' missing in base_damages.")
+            if not isinstance(base_damages[field], int):
+                raise TypeError(f"Field '{field}' is not an int ('{base_damages[field]}' of type '{type(base_damages[field])}' given instead).")
+            if base_damages[field] < 0:
+                raise ValueError(f"Field '{field}' should be non negative ('{base_damages[field]}' given instead).")
         
-        self.damages[characteristic] = damages
+        self.base_damages[characteristic] = base_damages
+
 
     def get_crit_chance(self):
         return self.crit_chance
@@ -51,6 +53,7 @@ class Spell():
 
         self.crit_chance = float(crit_chance)
 
+
     def get_uses_per_target(self):
         return self.uses_per_target
 
@@ -61,6 +64,7 @@ class Spell():
             raise ValueError(f"Uses per target should be -1 or a positive int ('{uses_per_target}' given instead).")
         
         self.uses_per_target = uses_per_target
+
 
     def get_uses_per_turn(self):
         return self.uses_per_turn
@@ -76,7 +80,7 @@ class Spell():
 
     @classmethod
     def check_json_validity(cls, json_data):
-        for key in ('damages', 'crit_chance', 'uses_per_target', 'uses_per_turn'):
+        for key in ('base_damages', 'crit_chance', 'uses_per_target', 'uses_per_turn'):
             if not key in json_data:
                 raise KeyError(f"JSON string does not contain a '{key}' key.")
         
@@ -96,16 +100,16 @@ class Spell():
             raise ValueError(f"json_data['uses_per_turn'] should be -1 or a positive int ('{json_data['uses_per_turn']}' given instead).")
         
         for characteristic in Characteristics:
-            if not characteristic in json_data['damages']:
-                raise KeyError(f"JSON string 'damages' array does not contains '{characteristic}'.")
+            if not characteristic in json_data['base_damages']:
+                raise KeyError(f"JSON string 'base_damages' array does not contains '{characteristic}'.")
             for field in ('min', 'max', 'crit_min', 'crit_max'):
-                if not field in json_data['damages'][characteristic]:
-                    raise KeyError(f"Field '{field}' missing in json_data['damages'][{characteristic}].")
-                field_value = json_data['damages'][characteristic][field]
+                if not field in json_data['base_damages'][characteristic]:
+                    raise KeyError(f"Field '{field}' missing in json_data['base_damages'][{characteristic}].")
+                field_value = json_data['base_damages'][characteristic][field]
                 if not isinstance(field_value, int):
-                    raise TypeError(f"json_data['damages'][{characteristic}][{field}] is not an int ('{field_value}' of type '{type(field_value)}' given instead).")
+                    raise TypeError(f"json_data['base_damages'][{characteristic}][{field}] is not an int ('{field_value}' of type '{type(field_value)}' given instead).")
                 if field_value < 0:
-                    raise ValueError(f"json_data['damages'][{characteristic}][{field}] should be non negative ('{field_value}' given instead).")
+                    raise ValueError(f"json_data['base_damages'][{characteristic}][{field}] should be non negative ('{field_value}' given instead).")
         
 
     @classmethod
@@ -114,7 +118,7 @@ class Spell():
         cls.check_json_validity(json_data)
 
         stats = Spell(from_scratch=False)
-        stats.damages = json_data['damages']
+        stats.base_damages = json_data['base_damages']
         stats.crit_chance = float(json_data['crit_chance'])
         stats.uses_per_target = json_data['uses_per_target']
         stats.uses_per_turn = json_data['uses_per_turn']
--- test_spell.py
+++ test_spell.py
@@ -7,11 +7,11 @@ from stats import Characteristics
 class TestSpell(unittest.TestCase):
 
     def test_create_empty(self):
-        empty_damages = {characteristic: {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0} for characteristic in Characteristics}
+        empty_base_damages = {characteristic: {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0} for characteristic in Characteristics}
 
         spell = Spell()
 
-        self.assertDictEqual(spell.damages, empty_damages)
+        self.assertDictEqual(spell.base_damages, empty_base_damages)
         self.assertEqual(spell.crit_chance, 0.0)
         self.assertEqual(spell.uses_per_target, -1)
         self.assertEqual(spell.uses_per_turn, -1)
@@ -24,10 +24,10 @@ class TestSpell(unittest.TestCase):
 
     def test_create_from_incomplete_json(self):
         json_missing_all = '{}'
-        json_missing_scalar_parameter = '{"damages": {}}'
-        json_missing_damages = '{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1}'
+        json_missing_scalar_parameter = '{"base_damages": {}}'
+        json_missing_base_damages = '{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1}'
         # Double { and } because of .format
-        json_missing_one_characteristic = '{{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1, "damages": {0}}}'.format(
+        json_missing_one_characteristic = '{{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1, "base_damages": {0}}}'.format(
             {characteristic.value: {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0} for characteristic in Characteristics if characteristic != Characteristics.LUCK}
         ).replace("'", '"')
 
@@ -36,43 +36,43 @@ class TestSpell(unittest.TestCase):
         with self.assertRaises(KeyError):
             Spell.from_json_string(json_missing_scalar_parameter)
         with self.assertRaises(KeyError):
-            Spell.from_json_string(json_missing_damages)
+            Spell.from_json_string(json_missing_base_damages)
         with self.assertRaises(KeyError):
             Spell.from_json_string(json_missing_one_characteristic)
     
     def test_create_from_valid_json(self):
-        valid_json_string = '{{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1, "damages": {0}}}'.format(
+        valid_json_string = '{{"crit_chance": 0, "uses_per_target": -1, "uses_per_turn": -1, "base_damages": {0}}}'.format(
             {characteristic.value: {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0} for characteristic in Characteristics}
         ).replace("'", '"')
 
         Spell.from_json_string(valid_json_string)
     
-    def test_get_damages(self):
+    def test_get_base_damages(self):
         spell = Spell()
-        empty_damages = {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0}
+        empty_base_damages = {'min': 0, 'max': 0, 'crit_min': 0, 'crit_max': 0}
 
-        self.assertDictEqual(spell.get_damages(Characteristics.STRENGTH), empty_damages)
+        self.assertDictEqual(spell.get_base_damages(Characteristics.STRENGTH), empty_base_damages)
 
         with self.assertRaises(TypeError):
-            spell.get_damages("string")
+            spell.get_base_damages("string")
     
     def test_set_characteristic(self):
         spell = Spell()
         damage = {'min': 10, 'max': 20, 'crit_min': 12, 'crit_max': 22}
 
-        spell.set_damages(Characteristics.INTELLIGENCE, damage)
-        self.assertDictEqual(spell.get_damages(Characteristics.INTELLIGENCE), damage)
+        spell.set_base_damages(Characteristics.INTELLIGENCE, damage)
+        self.assertDictEqual(spell.get_base_damages(Characteristics.INTELLIGENCE), damage)
 
         with self.assertRaises(TypeError):
-            spell.set_damages("string", damage)
+            spell.set_base_damages("string", damage)
         with self.assertRaises(TypeError):
-            spell.set_damages(Characteristics.STRENGTH, 0)
+            spell.set_base_damages(Characteristics.STRENGTH, 0)
         with self.assertRaises(KeyError):
-            spell.set_damages(Characteristics.LUCK, {})
+            spell.set_base_damages(Characteristics.LUCK, {})
         with self.assertRaises(TypeError):
-            spell.set_damages(Characteristics.AGILITY, {'min': 10, 'max': 20, 'crit_min': "string", 'crit_max': 22})
+            spell.set_base_damages(Characteristics.AGILITY, {'min': 10, 'max': 20, 'crit_min': "string", 'crit_max': 22})
         with self.assertRaises(ValueError):
-            spell.set_damages(Characteristics.AGILITY, {'min': -10, 'max': 20, 'crit_min': 12, 'crit_max': 22})
+            spell.set_base_damages(Characteristics.AGILITY, {'min': -10, 'max': 20, 'crit_min': 12, 'crit_max': 22})
     
     def test_set_crit_chance(self):
         spell = Spell()
