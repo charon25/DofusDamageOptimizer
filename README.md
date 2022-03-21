1dfd38545a3330b27a577ebcdd967cd4f4039a7a
Paul Kern <pkern@laas.fr>
2022-03-21 09:31:46 +0100

    First structure of manager

---
+++ README.md
@@ -0,0 +1,35 @@
+## Commandes
+
+### Générales
+
+Enregistrer : `s`
+Définir options par défaut pour le calcul de dégâts : `def <param> <value>`
+Help : `h`
+Fermer : `q`
+
+### Stats
+
+Créer nouvelle page de stats : `st new`
+Modifier page de stats : `st mod <short_name>`
+Supprimer page de stats : `st rm <short_name>`
+
+### Sorts
+
+Créer nouveau sort : `sp new`
+Modifier sort : `sp mod <short_name>`
+Supprimer sort : `sp rm <short_name>`
+
+### Ensembles de sorts
+
+Créer nouvel ensemble : `ss new`
+Supprimer ensemble : `ss rm <short_name>`
+Ajouter un sort à un ensemble : `ss add <spell_short_name>`
+Retirer un sort d'un ensemble : `ss del <spell_short_name>`
+
+### Dégâts
+
+Calculer meilleurs dégâts pour un ensemble (options : nombre de PA, PO min et max, monocible/multicible/versatile) : `dmg <spell_set_short_name> [<param> <value>]+`
+Paramètres :
+ - Nombre de PA : `pa <value>`
+ - PO : `po [min <value>] [max <value>]`
+ - Type : `t <mono/multi/versa>`
--- manager.py
+++ manager.py
@@ -1,3 +1,66 @@
+import json
+import os
+from typing import Any, Callable, List
+
+from spell import Spell
+from spell_set import SpellSet
+from stats import Characteristics, Damages, Stats
+
 
 class Manager:
-    pass
+    GENERAL_INSTRUCTIONS = ('s', 'def', 'h', 'q')
+    STATS_INSTRUCTION = 'sp'
+    SPELL_INSTRUCTION = 'st'
+    SPELL_SET_INSTRUCTION = 'ss'
+    DAMAGES_INSTRUCTION = 'dmg'
+
+    def __init__(self, filename: str, print_method: Callable[[str], Any]) -> None:
+        self.print = print_method
+        self.stats = []
+        self.spells = []
+        self.spell_set = []
+
+        self.load_from_file(filename)
+
+
+    def load_from_file(self, filename):
+        pass
+
+
+    def execute_general_command(self, instr, args: List[str]):
+        pass
+
+    def execute_stats_command(self, args: List[str]):
+        pass
+
+    def execute_spell_command(self, args: List[str]):
+        pass
+
+    def execute_spell_set_command(self, args: List[str]):
+        pass
+
+    def execute_damages_command(self, args: List[str]):
+        pass
+
+    def execute_command(self, command: str):
+        if command == '':
+            raise ValueError('Command should be non empty.')
+
+        instr, *args = command.split(' ')
+
+        if instr in Manager.GENERAL_INSTRUCTIONS:
+            self.execute_general_command(instr, args)
+
+        if instr == Manager.STATS_INSTRUCTION:
+            self.execute_stats_command(args)
+
+        if instr == Manager.SPELL_INSTRUCTION:
+            self.execute_spell_command(args)
+
+        if instr == Manager.SPELL_SET_INSTRUCTION:
+            self.execute_spell_set_command(args)
+
+        if instr == Manager.DAMAGES_INSTRUCTION:
+            self.execute_damages_command(args)
+
+        self.print(f"Unknown command instruction : '{instr}'.")
